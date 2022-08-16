ccf21bd576f0dfb823097e0927f95c573f28ec59
Vincent Berthet <vincent@web-74.com>
2022-08-16 11:29:04 +0200

    Apply payment method on selection (Price is then displayed correctly when using payment fees)

--- src/components/creditCard/CreditCard.jsx
+++ src/components/creditCard/CreditCard.jsx
@@ -1,5 +1,7 @@
 import React, {useEffect} from 'react';
 import {shape, func} from 'prop-types';
+import _get from "lodash.get";
+import { __ } from '@hyva/react-checkout/i18n';
 
 import Card from '@hyva/react-checkout/components/common/Card/Card';
 import RadioInput from '@hyva/react-checkout/components/common/Form/RadioInput';
@@ -7,21 +9,31 @@ import RadioInput from '@hyva/react-checkout/components/common/Form/RadioInput';
 import {paymentMethodShape} from '@hyva/react-checkout/utils/payment';
 import useCheckoutFormContext from '@hyva/react-checkout/hook/useCheckoutFormContext';
 
+import usePaymentMethodFormContext from '@hyva/react-checkout/components/paymentMethod/hooks/usePaymentMethodFormContext';
+import usePaymentMethodCartContext from '@hyva/react-checkout/components/paymentMethod/hooks/usePaymentMethodCartContext';
 import MollieComponent from "../mollieComponent";
 import useMolliePayment from "../../hook/useMolliePayment";
 
-
 function CreditCard({method, selected, actions}) {
+    const { submitHandler} = usePaymentMethodFormContext();
     const methodCode = method.code;
     const isSelected = methodCode === selected.code;
 
     const { registerPaymentAction } = useCheckoutFormContext();
     const { placeCardOrder } = useMolliePayment({methodCode});
 
+    const { methodList } =
+        usePaymentMethodCartContext();
     useEffect(() => {
         registerPaymentAction(methodCode, placeCardOrder);
     }, [registerPaymentAction, methodCode, placeCardOrder]);
 
+    const handlePaymentMethodSelection = async (event) => {
+        const methodSelected = _get(methodList, `${event.target.value}.code`);
+        await actions.change(event);
+
+        await submitHandler(methodSelected);
+    }
     if (!isSelected) {
         return (
             <RadioInput
@@ -29,7 +41,7 @@ function CreditCard({method, selected, actions}) {
                 label={method.title}
                 name="paymentMethod"
                 checked={isSelected}
-                onChange={actions.change}
+                onChange={handlePaymentMethodSelection}
             />
         );
     }
@@ -42,17 +54,17 @@ function CreditCard({method, selected, actions}) {
                     label={method.title}
                     name="paymentMethod"
                     checked={isSelected}
-                    onChange={actions.change}
+                    onChange={handlePaymentMethodSelection}
                 />
                 {isSelected && (
                     <div className="mx-4 mb-8 mt-2">
                         <Card bg="darker">
                             <div className="-mt-4">
-                                <MollieComponent type="cardNumber" name="Card number" />
-                                <MollieComponent type="cardHolder" name="Card holder" />
+                                <MollieComponent type="cardNumber" name={__("Card number")} />
+                                <MollieComponent type="cardHolder" name={__("Card holder")} />
                                 <div className="flex gap-4">
-                                    <MollieComponent type="expiryDate" name="Expiry Date" />
-                                    <MollieComponent type="verificationCode" name="Verification Code" />
+                                    <MollieComponent type="expiryDate" name={__("Expiry Date")} />
+                                    <MollieComponent type="verificationCode" name={__("Verification Code")} />
                                 </div>
                             </div>
                         </Card>
--- src/components/remoteForm/RemoteForm.jsx
+++ src/components/remoteForm/RemoteForm.jsx
@@ -1,15 +1,19 @@
 import React, {useEffect} from 'react';
 import {shape, func} from 'prop-types';
+import _get from "lodash.get";
 
 import RadioInput from '@hyva/react-checkout/components/common/Form/RadioInput';
 
 import {paymentMethodShape} from '@hyva/react-checkout/utils/payment';
 import useCheckoutFormContext from '@hyva/react-checkout/hook/useCheckoutFormContext';
 
+import usePaymentMethodFormContext from '@hyva/react-checkout/components/paymentMethod/hooks/usePaymentMethodFormContext';
+import usePaymentMethodCartContext from '@hyva/react-checkout/components/paymentMethod/hooks/usePaymentMethodCartContext';
 import useMolliePayment from "../../hook/useMolliePayment";
 
 
 function RemoteForm({method, selected, actions}) {
+    const { submitHandler} = usePaymentMethodFormContext();
     const methodCode = method.code;
     const isSelected = methodCode === selected.code;
 
@@ -20,6 +24,15 @@ function RemoteForm({method, selected, actions}) {
         registerPaymentAction(methodCode, placeOrder);
     }, [registerPaymentAction, methodCode, placeOrder]);
 
+    const { methodList } =
+        usePaymentMethodCartContext();
+
+    const handlePaymentMethodSelection = async (event) => {
+        const methodSelected = _get(methodList, `${event.target.value}.code`);
+        await actions.change(event);
+
+        await submitHandler(methodSelected);
+    }
     if (!isSelected) {
         return (
             <RadioInput
@@ -27,7 +40,7 @@ function RemoteForm({method, selected, actions}) {
                 label={method.title}
                 name="paymentMethod"
                 checked={isSelected}
-                onChange={actions.change}
+                onChange={handlePaymentMethodSelection}
             />
         );
     }
@@ -40,7 +53,7 @@ function RemoteForm({method, selected, actions}) {
                     label={method.title}
                     name="paymentMethod"
                     checked={isSelected}
-                    onChange={actions.change}
+                    onChange={handlePaymentMethodSelection}
                 />
             </div>
         </div>
